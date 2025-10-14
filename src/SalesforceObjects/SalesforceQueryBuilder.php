8243db8908e24e9dad8d4ef1f0d519518c38e39e
Flinty <38919621+Flinty916@users.noreply.github.com>
2025-10-14 11:43:32 +0100

    Update SalesforceQueryBuilder.php
    Handle Included sub queries for IN clauses

--- src/SalesforceObjects/SalesforceQueryBuilder.php
+++ src/SalesforceObjects/SalesforceQueryBuilder.php
@@ -176,6 +176,9 @@ class SalesforceQueryBuilder
                 if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/', $value)) {
                     return $value;
                 }
+                if(preg_match('/^\(/', $value)) { 
+                    return $value;
+                }
             }
             return "'" . str_replace("'", "\\'", $value) . "'";
         };
